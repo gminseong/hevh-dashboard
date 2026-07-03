586dd998e6f7f0044e4e79db2c92ea31852159c0
gminseong <g.minseong@gmail.com>
2026-07-03 16:58:33 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1287,7 +1287,7 @@ def dashboard():
                             orientation="h",
                             color="loss_type_name",
                             color_discrete_map=TYPE_COLOR,
-                            height=max(500, min(len(ts) * 20, 600)),   # ★ 최대 800px 제한
+                            height=600),   # ★ 최대 800px 제한
                             labels={"loss_min":"손실(분)", "loss_type_name":"유형"})
                 ft.update_layout(
                     showlegend=False,
